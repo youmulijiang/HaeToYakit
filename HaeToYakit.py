deba2151e69f9e24028ef1944421276d76558bde
youmulijiang <111237463+youmulijiang@users.noreply.github.com>
2024-09-09 02:36:16 +0800

    Add files via upload

---
+++ HaeToYakit.py
@@ -0,0 +1,196 @@
+import yaml
+import json
+import argparse
+from io import TextIOWrapper
+import os
+
+from tkinter import ttk
+from tkinter import Menu
+from tkinter import filedialog
+from tkinter import Text
+from tkinter import filedialog
+from tkinter import messagebox
+import tkinter as tk
+
+print(f"""
+作者:柚木梨酱
+介绍:一个能够将hae插件规则转换为yakit规则配置的脚本
+github:https://github.com/youmulijiang
+知识大陆:地图大师的挖洞军火库
+""")
+
+parse = argparse.ArgumentParser()
+parse.add_argument("-f","--file",metavar="hae_rules.yaml",help="指定hae的配置文件",type=argparse.FileType(encoding="utf-8"))
+parse.add_argument("-o","--output",metavar="savePath",help="保存指定目录",type=str,default=None)
+parse.add_argument("--gui",help="开启gui界面",default=None,action="store_true")
+args = parse.parse_args();
+
+def yamltojson(fileio:TextIOWrapper,savePath:str): 
+    # 该函数通过解析yaml格式的数据,返回json结果
+    allDictList = []
+    result = yaml.load(fileio.read(),Loader=yaml.FullLoader)
+    index:int = 0
+    groupList:list = result['rules']
+    for i in groupList:
+        ruleDict:dict = i 
+        ruleList:dict = ruleDict.get('rule')
+        ruleGroup:str = ruleDict.get('group')
+        for rule in ruleList:
+            index += 1
+            name = rule['name']
+            regex = rule['regex']
+            loaded = rule['loaded']
+            scope = rule['scope']
+            color = rule['color']
+            yakitDict = bpRuleToYakitRule(ruleGroup,name,regex,loaded,scope,color,index)
+            # print(yakitDict)
+            allDictList.append(yakitDict)
+    yakitJson = json.dumps(allDictList,indent=5,sort_keys=True)
+
+    if(savePath != None):
+        with open(savePath,"w+") as saveFile:
+            saveFile.write(yakitJson)
+    
+    return yakitJson
+        
+
+def bpRuleToYakitRule(ruleGroup:str,name:str,rule:str,loaded:bool,scope:str,color:str,index:int) -> dict:
+    """
+    对hae的配置文件进行解析,转换为yakit的规则
+    {
+        "Rule": "(?i)((access|admin|api|debug|auth|authorization|gpg|ops|ray|deploy|s3|certificate|aws|app|application|docker|es|elastic|elasticsearch|secret)[-_]{0,5}(key|token|secret|secretkey|pass|password|sid|debug))|(secret|password)([\"']?\\s*:\\s*|\\s*=\\s*)",
+        "NoReplace": true,
+        "Color": "red",
+        "EnableForRequest": true,
+        "EnableForResponse": true,
+        "EnableForHeader": true,
+        "EnableForBody": true,
+        "Index": 3,
+        "ExtraTag": [
+            "敏感信息"
+        ]
+    }
+    """
+    yakitDict = {}
+    yakitDict['ExtraTag'] = [ruleGroup+"/"+name]
+    yakitDict['VerboseName'] = name
+    yakitDict['Rule'] = rule
+    yakitDict['NoReplace'] = True
+    yakitDict['Color'] = color
+    yakitDict['Index'] = index
+    if(scope.endswith("body")):
+        yakitDict["EnableForBody"] = True
+
+    elif(scope.endswith("header")):
+        yakitDict['EnableForHeader'] = True
+    else:
+        yakitDict["EnableForBody"] = True
+        yakitDict['EnableForHeader'] = True
+
+    if(scope.startswith("request")):
+        yakitDict['EnableForRequest'] = True
+    elif(scope.startswith("response")):
+        yakitDict['EnableForResponse'] = True
+    else:
+        yakitDict['EnableForResponse'] = True
+        yakitDict['EnableForRequest'] = True
+    
+    return yakitDict
+
+class Root(ttk.Frame):
+    #添加gui操作
+    def __init__(self,master=None):
+        super().__init__()
+        # 创建í一个 ttk 样式的实例
+        style = ttk.Style()
+
+        # 使用样式配置主题
+        style.theme_use("xpnative")
+        self.createMenu()
+        self.master.title("HaeToYakit")
+        # self.haeYamlFileName = None
+        self.createYamlTextArea().grid(row=0,column=1,padx=15,pady=15)
+        self.createFuncArea().grid(row=0,column=2,padx=15,pady=15)
+        self.createJsonTextArea().grid(row=0,column=3,padx=15,pady=15)
+
+    def createMenu(self):
+        topMenu = Menu(self.master)
+        fileMenu = Menu(topMenu)
+        topMenu.add_cascade(label="菜单",menu=fileMenu)
+        self.master.config(menu=topMenu)
+        fileMenu.add_command(label="作者",command=lambda:self._getAuthor())
+        fileMenu.add_command(label="介绍",command=lambda:self._getIntroduce())
+
+    def createYamlTextArea(self):
+        self.yamlTextArea = Text(width=30,height=40)
+        return self.yamlTextArea
+    
+    def createJsonTextArea(self):
+        self.jsonTextArea = Text(width=30,height=40)
+        return self.jsonTextArea
+    
+    def createFuncArea(self):
+        funcAreaFrame = ttk.Frame(self.master)
+        self.addHaeRuleButton = ttk.Button(master=funcAreaFrame,text="打开hae规则文件",command=lambda:self._getHaeRuleFile())
+        self.parseHaeRuleToYakitButton = ttk.Button(master=funcAreaFrame,text="转换为yakit配置格式",command=lambda:self._parseYamlToJson())
+        self.saveYakitJsonButton = ttk.Button(master=funcAreaFrame,text="保存配置文件",command=lambda:self._saveYakitJson())
+
+        self.addHaeRuleButton.grid(row=0,column=0)
+        self.parseHaeRuleToYakitButton.grid(row=1,column=0)
+        self.saveYakitJsonButton.grid(row=2,column=0)
+        return funcAreaFrame
+        
+
+    
+    def _getHaeRuleFile(self):
+        try:
+            self.yamlTextArea.delete("1.0",tk.END)
+            self.haeYamlFileName = filedialog.askopenfilename(filetypes=[("hae规则文件",".yaml")],title="请选择hae配置文件")
+            with open(self.haeYamlFileName,encoding="utf-8") as yamlFile:
+                for i in yamlFile:
+                    self.yamlTextArea.insert("insert",i)
+        except:
+            pass
+
+    def _parseYamlToJson(self):
+        self.jsonTextArea.delete("1.0",tk.END)
+        yakitJson = yamltojson(open(self.haeYamlFileName,encoding="utf-8"),None)
+        for i in yakitJson:
+            self.jsonTextArea.insert("insert",i)
+
+    def _getAuthor(self):
+        text = """ 
+        作者:柚木梨酱
+        github:https://github.com/youmulijiang
+        知识大陆:地图大师的挖洞军火库
+         """
+        messagebox.showinfo(title="作者",message=text)
+
+    def _getIntroduce(self):
+        text = "一个能够将hae插件规则转换为yakit规则配置的脚本"
+        messagebox.showinfo(title="介绍",message=text)
+
+    def _saveYakitJson(self):
+        try:
+            yamlFileName = filedialog.askopenfilename()
+            ok = messagebox.askyesnocancel(message=f"是否保存到{yamlFileName}")
+            if(ok):
+                with open(yamlFileName,mode="w+",encoding="utf-8") as yamlFile:
+                    yamlFile.write(self.yamlTextArea.get("1.0","end-1c"))
+        except:
+            pass
+
+        
+if __name__ == "__main__":
+    try:
+        if(args.file == None and args.output == None and args.gui == None):
+            root = Root()
+            root.mainloop()
+        if(args.gui):
+            root = Root()
+            root.mainloop()
+        if (args.file != None or args.output != None):
+            print(yamltojson(args.file,args.output))
+    except:
+        pass
+    
---
+++ readme.md
@@ -0,0 +1,47 @@
+# HaeToYakit
+
+## 介绍
+
+HaeToYakit是一个将hae配置文件转换为yakit适配的配置文件脚本
+
+hae是burpsuite的一个强大的插件，在挖掘src中起到快速定位敏感信息，辅助挖掘的作用。但是，梨酱根据群里的师傅的反馈，发现hae插件在burpsuite存在大流量或者数据包太大时会产生卡顿，于是想如果将hae的规则配置文件转换为yakit的规则配置文件，由yakit负责hae的功能，这样可以减轻大流量或者大数据造成的gui界面卡顿，毕竟yakit底层是由go编写的，在处理高并发，大流量有天然优势，而且该脚本也能进一步完善yakit的生态。
+
+## 使用
+
+```
+usage: HaeToYakit.py [-h] [-f hae_rules.yaml] [-o savePath] [--gui]
+
+options:
+  -h, --help            show this help message and exit
+  -f hae_rules.yaml, --file hae_rules.yaml
+                        指定hae的配置文件
+  -o savePath, --output savePath
+                        保存指定目录
+  --gui                 开启gui界面
+
+```
+
+该脚本带有一个gui界面,如果使用脚本时不添加任何参数时,那么将会自动开启gui模式
+
+## 教程
+
+1. 开启脚本
+
+   ![1725818953449](image/readme/1725818953449.png)
+2. 点击打开规则文件，选择好hae规则文件
+   ![1725819359646](image/readme/1725819359646.png)
+3. 点击转换为yakit配置格式
+   ![1725819385900](image/readme/1725819385900.png)
+4. 点击保存配置文件，选择保存路径，或者点击yakit配置文件面板，ctrl+a进行全选，将其复制到剪贴板中
+5. 开启yakit，点击mitm劫持
+   ![1725819585703](image/readme/1725819585703.png)
+6. 点击规则配置，然后在点击导入配置，将规则文件导入
+   ![1725819672074](image/readme/1725819672074.png)
+7. 点击导入，就可以使用了
+
+
+## 作者
+
+作者是一个普通的安全开发成员，如果这个项目对你有帮助的话，请点击右上角的⭐
+
+梨酱最喜欢⭐⭐啦 ヾ(≧▽≦*)o*
